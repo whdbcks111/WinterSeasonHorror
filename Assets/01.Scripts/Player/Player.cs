4b1ffe0e25c94ccb69d59415ca6ca2e484859ccd
화베(조유찬) <whdbcks111@gmail.com>
2024-01-08 16:42:33 +0900

    Player run added

--- Assets/01.Scripts/Player/Player.cs
+++ Assets/01.Scripts/Player/Player.cs
@@ -13,21 +13,35 @@ public class Player : MonoBehaviour
 
     [SerializeField] private Vector2 _camOffset;
 
+    [SerializeField] private Color _hiddenColor;
     [SerializeField] private float _moveSpeed = 5;
+    [SerializeField] private float _runModifier = 1.3f;
     [SerializeField] private float _jumpForce = 10;
     [SerializeField] private int _maxJumpCount = 1;
+    
     public float MaxLightEnerge = 100;
     [HideInInspector] public float LightEnerge;
+
+    public float MaxStamina = 20f;
+    [HideInInspector] public float Stamina;
 
     private Rigidbody2D _rigid;
     private Animator _animator;
     private SpriteRenderer _spriteRenderer;
-    private int _jumpCount = 0;
+
     private readonly HashSet<Collider2D> _steppingGrounds = new();
     private bool _isLeftDir = false;
+
+    private int _jumpCount = 0;
+    private bool _isJumping = false;
+    private bool _isLeftJump = false;
+
     private float _handLightDefaultAngleX;
     private float _handLightOffsetX, _surroundLightOffsetX;
 
+    private bool _isHidden = false;
+
+    public bool IsHidden { get => _isHidden; }
     public bool IsLightOn { get => _handLight.gameObject.activeSelf; }
     public bool IsOnGround { get => _steppingGrounds.Count > 0; }
     public float MoveSpeed { get => _moveSpeed; }
@@ -36,13 +50,14 @@ public class Player : MonoBehaviour
     {
         Instance = this;
         LightEnerge = MaxLightEnerge;
+        Stamina = MaxStamina;
         _handLightDefaultAngleX = _handLight.transform.eulerAngles.z;
         _rigid = GetComponent<Rigidbody2D>();
         _animator = GetComponent<Animator>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
 
-        _handLightOffsetX = _handLight.transform.position.x;
-        _surroundLightOffsetX = _surroundLight.transform.position.x;
+        _handLightOffsetX = _handLight.transform.position.x - transform.position.x;
+        _surroundLightOffsetX = _surroundLight.transform.position.x - transform.position.x;
     }
 
     private void Start()
@@ -55,22 +70,43 @@ public class Player : MonoBehaviour
         JumpUpdate();
         LightUpdate();
         MoveUpdate();
+        HiddenViewUpdate();
+    }
+
+    public void Hide() => _isHidden = true;
+    public void Reveal() => _isHidden = false;
+
+    private void HiddenViewUpdate()
+    {
+        _spriteRenderer.color = IsHidden ? _hiddenColor : Color.white;
+
     }
 
     private void MoveUpdate()
     {
+        var isRunning = Input.GetKey(KeyCode.LeftShift) && Stamina > 0f;
+
         var xAxis = Input.GetAxisRaw("Horizontal");
+        if(_isJumping && (xAxis > 0f && _isLeftJump || xAxis < 0f && !_isLeftJump))
+        {
+            xAxis = 0f;
+        }
+
         if (!Mathf.Approximately(xAxis, 0f)) _isLeftDir = xAxis < 0f;
         var velX = xAxis * _moveSpeed;
 
+        if(isRunning)
+        {
+            velX *= _runModifier;
+            Stamina -= Time.deltaTime;
+        }
+
+        _animator.SetBool("IsRunning", isRunning);
         _animator.SetFloat("VelX", velX);
         _animator.SetBool("IsOnGround", IsOnGround);
         _animator.SetFloat("VelY", _rigid.velocity.y);
 
-        var befPos = _rigid.position;
         transform.Translate(velX * Time.deltaTime * Vector2.right);
-        Physics2D.SyncTransforms();
-        print($"{befPos} {_rigid.position}");
 
         _spriteRenderer.flipX = _isLeftDir;
         CameraController.Instance.SetOffset(_camOffset * (_isLeftDir ? -1 : 1), 0.7f);
@@ -78,11 +114,11 @@ public class Player : MonoBehaviour
 
     private void LightUpdate()
     {
-
         if(Input.GetKeyDown(KeyCode.W))
         {
             _handLight.gameObject.SetActive(!_handLight.gameObject.activeSelf);
         }
+        if(LightEnerge <= 0f) _handLight.gameObject.SetActive(false);
 
         if (IsLightOn) LightEnerge -= Time.deltaTime;
 
@@ -94,7 +130,9 @@ public class Player : MonoBehaviour
 
     private void JumpUpdate()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (IsOnGround && _rigid.velocity.y < 0.01f) _isJumping = false;
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             Jump();
         }
@@ -102,10 +140,12 @@ public class Player : MonoBehaviour
 
     public void Jump()
     {
-        _animator.SetTrigger("Jump");
         if(IsOnGround && _jumpCount > 0)
         {
+            _isJumping = true;
             _jumpCount--;
+            _animator.SetTrigger("Jump");
+            _isLeftJump = _isLeftDir;
             _rigid.velocity = new(_rigid.velocity.x, _jumpForce);
         }
     }
