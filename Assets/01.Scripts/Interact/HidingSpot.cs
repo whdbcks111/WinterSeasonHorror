d38a7fc2b6758555a7b2bab4127a2fe191ef86e0
LDY <dkjk1234@naver.com>
2024-01-09 19:04:47 +0900

    InterractableObect_Lever_HideArea

--- Assets/01.Scripts/Interact/HidingSpot.cs
+++ Assets/01.Scripts/Interact/HidingSpot.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InteractableObject  : MonoBehaviour
+public class HidingSpot : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
