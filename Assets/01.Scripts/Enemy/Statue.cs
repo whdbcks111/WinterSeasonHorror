b0bbe74a967df64645eb2e165f97c558d23125b1
SeungBeom <sbseungbeom99@gmail.com>
2024-01-15 18:55:03 +0900

    Statue Advanced.
    이거 확인하면서 설명도 보나요?

--- Assets/01.Scripts/Enemy/Statue.cs
+++ Assets/01.Scripts/Enemy/Statue.cs
@@ -11,18 +11,25 @@ public class Statue : MonoBehaviour
 
     Believer _believer;
     Player _player;
-    [SerializeField] private Vector2 _boxSize;
-    [SerializeField] private Transform _boxPosition;
+    [SerializeField] private Vector2 _alarmBoxSize;
+    [SerializeField] private Transform _alarmBoxPosition;
+    [SerializeField] private Vector2 _searchBoxSize;
+    [SerializeField] private Transform _searchBoxPosition;
 
     [SerializeField] private GameObject _eyeSphere;
     [SerializeField] private GameObject _pupil;
 
+    [SerializeField] private float _eyeMaxDistance;
+    [SerializeField] private float _smoothTime;
 
     [Header("수녀상 종류")]
     [SerializeField] GameObject[] _statuePresets;
     private GameObject _selectedStatue;
     [SerializeField] GameObject _statuePreview;
 
+    private bool _playerIsInRange;
+
+    private Vector3 _velocity = Vector3.zero;
     private void Start()
     {
         _player = Player.Instance;
@@ -40,35 +47,33 @@ public class Statue : MonoBehaviour
         {
             AttackOrder();
         }
-        if (Input.GetKeyDown(KeyCode.H))
+        SearchNear();
+
+        /*
+        if (_playerIsInRange && (transform.position.x > Player.Instance.transform.position.x) == Player.Instance.IsLeftDir)
         {
+            //플레이어가 등지고 있음.
             ActivateEye();
+            GazingPlayer();
         }
-        if (Input.GetKeyDown(KeyCode.B))
+        if (_playerIsInRange && (transform.position.x > Player.Instance.transform.position.x) != Player.Instance.IsLeftDir)
         {
+            //플레이어가 쳐다보고 있음.
             DeactivateEye();
         }
-
-
-
+        */
+        ActivateEye();
+        GazingPlayer();
     }
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.magenta;
-        Gizmos.DrawWireCube(_boxPosition.position, _boxSize);
+        Gizmos.DrawWireCube(_alarmBoxPosition.position, _alarmBoxSize);
+        Gizmos.DrawWireCube(_searchBoxPosition.position, _searchBoxSize);
     }
-
-    private void ActivateEye()
+    private void AttackOrder()
     {
-        _pupil.SetActive(true);
-    }
-    private void DeactivateEye()
-    {
-        _pupil.SetActive(false);
-    }
-    void AttackOrder()
-    {
-        RaycastHit2D[] hits = Physics2D.BoxCastAll(_boxPosition.position, _boxSize,0,Vector2.zero);
+        RaycastHit2D[] hits = Physics2D.BoxCastAll(_alarmBoxPosition.position, _alarmBoxSize,0,Vector2.zero);
         foreach(RaycastHit2D hit in hits)
         {
             if(hit.collider.gameObject.TryGetComponent<Believer>(out Believer BelieverScript) && BelieverScript._fsm.CurrentState != BelieverScript._threteningHide && BelieverScript._fsm.CurrentState != BelieverScript._chaseState)
@@ -77,28 +82,64 @@ public class Statue : MonoBehaviour
             }
         }
     }
+    void ActivateEye()
+    {
+        _eyeSphere.SetActive(true);
+        _pupil.SetActive(true);
+    }
+    void DeactivateEye()
+    {
+        _eyeSphere.SetActive(false);
+        _pupil.SetActive(false);
+    }
+    void GazingPlayer()
+    {
+        var Direction = (Player.Instance.transform.position - _eyeSphere.transform.position);
+        var PupilPos = Direction.normalized * Mathf.Clamp(Direction.magnitude, 0, _eyeMaxDistance);
+
+        Debug.Log(PupilPos);
+        _pupil.transform.localPosition = Vector3.SmoothDamp(_pupil.transform.localPosition, PupilPos, ref _velocity, _smoothTime);
+    }
+    void SearchNear()
+    {
+        RaycastHit2D[] hits = Physics2D.BoxCastAll(_searchBoxPosition.position, _searchBoxSize, 0, Vector2.zero);
+        foreach (RaycastHit2D hit in hits)
+        {
+            if (hit.collider.gameObject == Player.Instance.gameObject)
+            {
+                _playerIsInRange = true;
+            }
+            else _playerIsInRange = false;
+        }
+    }
     private GameObject SelectPreset()
     {
         int Selecter = Random.Range(0, _statuePresets.Length);
         GameObject SelectedPreset = Instantiate(_statuePresets[Selecter],transform.position,Quaternion.identity);
         SelectedPreset.transform.parent = gameObject.transform;
 
+        _pupil = Instantiate(_pupil, transform.position, Quaternion.identity);
+        _eyeSphere = Instantiate(_eyeSphere, transform.position, Quaternion.identity);
+
+        _pupil.transform.parent = gameObject.transform;
+        _eyeSphere.transform.parent = gameObject.transform;
+
         switch (Selecter) {
             case 0:
-                Instantiate(_pupil, transform.position, Quaternion.identity);
-                Instantiate(_eyeSphere, transform.position, Quaternion.identity);
+                _pupil.transform.position = new Vector2(-0.139f, 0.265f) + new Vector2(transform.position.x,transform.position.y);
+                _eyeSphere.transform.position = new Vector2(-0.139f, 0.265f) + new Vector2(transform.position.x, transform.position.y);
                 break;
             case 1:
-                Instantiate(_pupil, transform.position, Quaternion.identity);
-                Instantiate(_eyeSphere, transform.position, Quaternion.identity);
+                _pupil.transform.position = new Vector2(-0.142f, 0.133f) + new Vector2(transform.position.x, transform.position.y);
+                _eyeSphere.transform.position = new Vector2(-0.142f, 0.133f) + new Vector2(transform.position.x, transform.position.y);
                 break;
             case 2:
-                Instantiate(_pupil, transform.position, Quaternion.identity);
-                Instantiate(_eyeSphere, transform.position, Quaternion.identity);
+                _pupil.transform.position = new Vector2(-0.343f, 0.095f) + new Vector2(transform.position.x, transform.position.y);
+                _eyeSphere.transform.position = new Vector2(-0.343f, 0.095f) + new Vector2(transform.position.x, transform.position.y);
                 break;
             case 3:
-                Instantiate(_pupil, transform.position, Quaternion.identity);
-                Instantiate(_eyeSphere, transform.position, Quaternion.identity);
+                _pupil.transform.position = new Vector2(-0.111f, 0.306f) + new Vector2(transform.position.x, transform.position.y);
+                _eyeSphere.transform.position = new Vector2(-0.111f, 0.306f) + new Vector2(transform.position.x, transform.position.y);
                 break;
             default:
                 break;
