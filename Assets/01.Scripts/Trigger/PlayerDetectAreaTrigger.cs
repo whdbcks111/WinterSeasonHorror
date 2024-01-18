cb8506779b41e0590179052f38df75d0042dc8fb
화베(조유찬) <whdbcks111@gmail.com>
2024-01-18 10:39:26 +0900

    Detect Bounds gizmos added

--- Assets/01.Scripts/Trigger/PlayerDetectAreaTrigger.cs
+++ Assets/01.Scripts/Trigger/PlayerDetectAreaTrigger.cs
@@ -3,6 +3,19 @@ using UnityEngine;
 [RequireComponent(typeof(Collider2D))]
 public class PlayerDetectAreaTrigger : DetectAreaTrigger
 {
+    public bool ShowTriggerBounds = true;
+
+    private void OnDrawGizmos()
+    {
+        if(!ShowTriggerBounds) return;
+        var colliders = GetComponentsInChildren<Collider2D>();
+        Gizmos.color = new Color(0f, 1f, 1f);
+        foreach (var collider in colliders)
+        {
+            Gizmos.DrawWireCube(collider.bounds.center, collider.bounds.size);
+        }
+    }
+
     protected override bool CheckCollision(Collider2D collider)
     {
         return collider.TryGetComponent(out Player _);
