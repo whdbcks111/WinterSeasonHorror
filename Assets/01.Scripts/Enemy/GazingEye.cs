5e6c90f5dceca7254f4e6305619114f95eda2c62
SeungBeom <sbseungbeom99@gmail.com>
2024-01-18 18:21:26 +0900

    Statue Prefab Applied , Enemy Light Added.

--- Assets/01.Scripts/Enemy/GazingEye.cs
+++ Assets/01.Scripts/Enemy/GazingEye.cs
@@ -13,6 +13,8 @@ public class GazingEye : MonoBehaviour
     private float _scaleValue;
 
     // Start is called before the first frame update
+
+    
     void Start()
     {
         _eyeSphere = gameObject;
